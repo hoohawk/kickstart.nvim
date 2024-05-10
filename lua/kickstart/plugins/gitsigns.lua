4e99b80ac3bc9f1b67a929e5c3ebd140f3d1ca58
Hao Hu <hao@zhuventures.com>
2024-05-09 19:23:00 -0700

    update

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -1,12 +1,14 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 -- NOTE: gitsigns is already included in init.lua but contains only the base
 -- config. This will add also the recommended keymaps.
+-- for all supported cmd, use `:Gitsigns ....`
 
-return { -- for all supported cmd, use `:Gitsigns ....`
+return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
-      current_line_blame = true,
+      current_line_blame = true, -- default to show blame, use keymap to toggle
+
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
