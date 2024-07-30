e174c05f22c5e6095eb4dab2092722fea65add73
Hao Hu <hao@zhuventures.com>
2024-07-30 11:19:37 -0700

    update harpoon

--- lua/kickstart/plugins/harpoon.lua
+++ lua/kickstart/plugins/harpoon.lua
@@ -53,25 +53,43 @@ return {
     -- end, { desc = 'Open harpoon window' })
     vim.keymap.set('n', '<leader>e', function()
       toggle_telescope(harpoon:list())
-    end, { desc = 'Harpoon Explorer' })
+    end, { desc = 'Harpoon: Explorer' })
+
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'Harpoon quick menu' })
-    vim.keymap.set('n', '<C-m>', function()
+    end, { desc = 'Harpoon: quick menu' })
+
+    vim.keymap.set('n', '<C-h>', function()
       harpoon:list():add()
-    end, { desc = 'Harpoon mark' })
+    end, { desc = 'Harpoon: harpoon it' })
+
+    -- vim.keymap.set('n', '<C-m>', function()
+    --   harpoon:list():add()
+    -- end, { desc = 'Harpoon: mark' })
+
     vim.keymap.set('n', '<leader>1', function()
       harpoon:list():select(1)
-    end, { desc = 'Harpoon file 1' })
+    end, { desc = 'Harpoon: file 1' })
+
     vim.keymap.set('n', '<leader>2', function()
       harpoon:list():select(2)
-    end, { desc = 'Harpoon file 2' })
+    end, { desc = 'Harpoon: file 2' })
+
     vim.keymap.set('n', '<leader>3', function()
       harpoon:list():select(3)
-    end, { desc = 'Harpoon file 3' })
+    end, { desc = 'Harpoon: file 3' })
+
     vim.keymap.set('n', '<leader>4', function()
       harpoon:list():select(4)
-    end, { desc = 'Harpoon file 4' })
+    end, { desc = 'Harpoon: file 4' })
+
+    vim.keymap.set('n', '<C-p>', function()
+      harpoon:list():prev()
+    end, { desc = 'Harpoon: previous' })
+
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():next()
+    end, { desc = 'Harpoon: next' })
   end,
   event = 'VeryLazy', -- or any other event you might want to use.
 }
