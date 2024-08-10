90680c52713e1a5f24cd2cfd9deb402edf9ba944
Hao Hu <hao@zhuventures.com>
2024-08-09 17:02:24 -0700

    update

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -167,6 +167,8 @@ require('lazy').setup(
 
     require 'kickstart.plugins.outline',
 
+    require 'kickstart.plugins.neogit',
+
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
     --
