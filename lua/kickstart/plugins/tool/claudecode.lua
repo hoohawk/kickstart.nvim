a5f0efeb2a9dce880fe29a4d8e4b888db68bf970
Hao Hu <hao@zhuventures.com>
2025-12-30 22:30:37 -0800

    Revert "refactor: extract terminal config from claudecode.lua"
    This reverts commit 2a49b73ea3865c99eaf811d45681b24500475a20.

--- lua/kickstart/plugins/tool/claudecode.lua
+++ lua/kickstart/plugins/tool/claudecode.lua
@@ -1,9 +1,30 @@
 return {
   'coder/claudecode.nvim',
+  dependencies = {
+    {
+      'folke/snacks.nvim',
+      opts = {
+        terminal = {
+          win = {
+            style = 'terminal',
+            wo = {
+              winblend = 0,
+              winfixwidth = false,
+            },
+          },
+        },
+      },
+    },
+  },
   config = function()
     require('claudecode').setup {
       terminal_cmd = '~/.local/bin/claude',
     }
+    -- Terminal resize keymaps (Shift + arrow keys, 2x speed)
+    vim.keymap.set('t', '<S-Left>', '<C-\\><C-n>2<C-w><i', { desc = 'Shrink terminal width' })
+    vim.keymap.set('t', '<S-Right>', '<C-\\><C-n>2<C-w>>i', { desc = 'Enlarge terminal width' })
+    vim.keymap.set('t', '<S-Up>', '<C-\\><C-n>2<C-w>+i', { desc = 'Increase terminal height' })
+    vim.keymap.set('t', '<S-Down>', '<C-\\><C-n>2<C-w>-i', { desc = 'Decrease terminal height' })
   end,
   lazy = false,
   keys = {
@@ -22,5 +43,10 @@ return {
     },
     { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
     { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+    -- Terminal navigation
+    { '<C-h>', '<cmd>TmuxNavigateLeft<cr>', desc = 'Navigate left', mode = 't' },
+    { '<C-j>', '<cmd>TmuxNavigateDown<cr>', desc = 'Navigate down', mode = 't' },
+    { '<C-k>', '<cmd>TmuxNavigateUp<cr>', desc = 'Navigate up', mode = 't' },
+    { '<C-l>', '<cmd>TmuxNavigateRight<cr>', desc = 'Navigate right', mode = 't' },
   },
 }
